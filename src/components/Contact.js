c3f8f434a25524a99bfa35920347df5682c0ba8c
Joshua Humphrey <joshhumphrey1@gmail.com>
2019-02-20 00:20:11 -0600

    essage

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,40 +9,40 @@ const Contact = (props) => {
 		return (
 			<div className='contact-form'>
 			<h4>If you are interested in working together, contact me here. I will be in touch shortly!</h4>
-			<form name='contact' method='POST' data-netlify='true'>
-		      <input type="hidden" name="form-name" value="contact" />
+			<form method='POST' data-netlify='true'>
+			    <input type="hidden" name="form-name" value="contact" />
 
-			<input 
-			type='text' 
-			id='name'
-			name='name'
-			placeholder='name'
-			onChange={props.handleChange}
-			/>
-			<br/>
-			<input 
-			type='text'
-			id='email'
-			name='email' 
-			placeholder='email'
-			onChange={props.handleChange}
-			/>
-			<br/>
-			<input 
-			type='text' 
-			id='subject'
-			name='subject'
-			placeholder='subject'
-			onChange={props.handleChange}
-			/>
-			<br/>
-			<textarea
-			id='message'
-			name='message'
-			placeholder='Message here...'
-			onChange={props.handleChange} />
-			<br/>
-			<button className='submit-button'>Submit</button>
+				<input 
+				type='text' 
+				id='name'
+				name='name'
+				placeholder='name'
+				onChange={props.handleChange}
+				/>
+				<br/>
+				<input 
+				type='text'
+				id='email'
+				name='email' 
+				placeholder='email'
+				onChange={props.handleChange}
+				/>
+				<br/>
+				<input 
+				type='text' 
+				id='subject'
+				name='subject'
+				placeholder='subject'
+				onChange={props.handleChange}
+				/>
+				<br/>
+				<textarea
+				id='message'
+				name='message'
+				placeholder='Message here...'
+				onChange={props.handleChange} />
+				<br/>
+				<button className='submit-button'>Submit</button>
 			</form>
 
 			</div>
